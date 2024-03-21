5a73dc6c8655322a3a396d6244a0420ff0ab9758
LoganHansen20 <113001105+Dirtbike140@users.noreply.github.com>
2024-03-21 10:11:59 -0500

    Made the start of the official website

--- nuthatch/src/App.js
+++ nuthatch/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import SignIn from './components/auth/SignIn';
 import SignUp from './components/auth/SignUp';
@@ -10,21 +9,6 @@ function App() {
       <SignIn />
       <SignUp />
       <AuthDetails />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      
     </div>
   );
 }
