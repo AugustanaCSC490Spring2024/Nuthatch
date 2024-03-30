236f95da5f06910b66b96b77eb126c2d019be440
mayaconway <mayaconway21@augustana.edu>
2024-03-30 15:15:19 -0500

    Added a responsive hamburger menu to the webpage that shows when minimized, not yet fully working on clicks to show navbar

---
+++ Si Website/index.js
@@ -0,0 +1,8 @@
+/*https://www.youtube.com/watch?v=flItyHiDm7E&ab_channel=WebDevTutorials*/
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navLinks.classList.toggle("active");
+});
