9246a024b439b8429402c39e07061ea836739f45
mayaconway <mayaconway21@augustana.edu>
2024-04-01 15:39:19 -0500

    Jack and Maya created about page

--- Si Website/index.js
+++ Si Website/index.js
@@ -1,8 +1,8 @@
 /*https://www.youtube.com/watch?v=flItyHiDm7E&ab_channel=WebDevTutorials*/
 const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".nav-links");
+const navMenu = document.querySelector(".nav-menu");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
-  navLinks.classList.toggle("active");
+  navMenu.classList.toggle("active");
 });
