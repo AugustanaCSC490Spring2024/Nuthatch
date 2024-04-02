d118b6fb2dcb49de104b14a5079cf769625e74b2
LoganHansen20 <113001105+Dirtbike140@users.noreply.github.com>
2024-04-01 23:17:13 -0500

    Not working yet payment method using stripe. Firebase is connected to stripe
    https://youtube.com/playlist?list=PLC5vixW_4xSIGczDXjwOEQilG2O6N6-kD&feature=shared

--- payments-test/src/App.js
+++ payments-test/src/App.js
@@ -1,25 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+import AuthDetails from './components/AuthDetails';
+import React, { useState, useEffect } from 'react';
+import db, { auth } from '../firebase';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SignIn/>
+      <SignUp/>
+      <AuthDetails/>
     </div>
   );
 }
+
+const Home = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    db.collection('products').where('active', '==', true).get().then(snapshot => {
+      console.log('snapshot', snapshot);
+      })
+  } , [])
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  )
+}
+
 
 export default App;
