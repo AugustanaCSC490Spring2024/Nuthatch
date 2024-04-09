05e7f62b6d4011d503cc1767c9653628a3e7a8cf
JackKiefer <JackKiefer21@augustana.edu>
2024-04-09 09:52:55 -0500

    File cleanup, reworked firebase code

--- nuthatch/public/burger.js
+++
@@ -1,8 +0,0 @@
-/*https://www.youtube.com/watch?v=flItyHiDm7E&ab_channel=WebDevTutorials*/
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
-});
