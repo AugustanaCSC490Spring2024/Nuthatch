cadda7d86e97a0616876712d8ccd6ff69339b0ac
LoganHansen20 <113001105+Dirtbike140@users.noreply.github.com>
2024-04-11 13:40:43 -0500

    Started CVS files with Stonedhal

--- nuthatch main/src/pages/home.js
+++ nuthatch main/src/pages/home.js
@@ -1,8 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import gymnast from './0.png'
+import fs from "fs";
 
 function Home(){
+    const fs = require("fs");
+    const { parse } = require("csv-parse");
+    const data = [];
+
+    fs.createReadStream("./Demo1.csv")
+    .pipe(
+        parse({
+        delimiter: ",",
+        columns: true,
+        ltrim: true,
+        })
+    )
+    .on("data", function (row) {
+        // This will push the object row into the array
+        data.push(row);
+    })
+    .on("error", function (error) {
+        console.log(error.message);
+    })
+    .on("end", function () {
+        // Here log the result array
+        console.log("parsed csv data:");
+        console.log(data);
+    });
+
     return (
         
         <div class="container">
