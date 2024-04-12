c51b57c35adb29497f7a63b833765d15206b0f63
LoganHansen20 <113001105+Dirtbike140@users.noreply.github.com>
2024-04-12 13:23:11 -0500

    Stonedahl helped download CSV from Firebase storage and parse CSV to data array

--- nuthatch main/src/App.js
+++ nuthatch main/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import SignIn from './components/auth/SignIn.jsx';
 import SignUp from './components/auth/SignUp.jsx';
@@ -6,29 +5,52 @@ import AuthDetails from './components/AuthDetails';
 import Home from './pages/home.js';
 import About from './pages/about.js';
 import Navbar from './components/Navbar.js';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { set } from 'firebase/database';
+import { usePapaParse } from 'react-papaparse';
 
-// import {
-//     BrowserRouter as Router,
-//     Routes,
-//     Route,
-//     Navigate,
-// } from "react-router-dom";
+import { getStorage, ref, getDownloadURL, getBlob } from "firebase/storage";
+
 
 function App() {
+  const [csvData, setCSVData] = useState("no data yet");
+
+  const { readString } = usePapaParse();
+
+  useEffect(() => {
+    const fetchData = async () => { 
+      const pathReference = ref(getStorage(), 'Demo1/Demo1.csv');
+      const blob = await getBlob(pathReference);
+      const csvText = await blob.text();
+      readString(csvText, 
+                  {   header: true,
+                      worker: true,
+                      complete: (results) => {
+                        console.log("Parsing complete:", results);
+                        setCSVData(results.data);
+                      }
+                  });
+    }
+
+    fetchData();
+  }, []);
+  
+  
+
   return (
     <section class="header">
     <div className="App">
       <Router>
         <Navbar/>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home data={csvData} />} />
           <Route path="/signin" element={<SignIn /> } />
           <Route path="/signup" element={<SignUp />} />
           <Route path='/about' element={<About />} />
         </Routes>
       </Router>
+      {/* <div>{JSON.stringify(csvData)}</div> */}
     </div>
     </section>
   );
--- nuthatch main/src/pages/home.js
+++ nuthatch main/src/pages/home.js
@@ -1,34 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import gymnast from './0.png'
-import fs from "fs";
 
-function Home(){
-    const fs = require("fs");
-    const { parse } = require("csv-parse");
-    const data = [];
 
-    fs.createReadStream("./Demo1.csv")
-    .pipe(
-        parse({
-        delimiter: ",",
-        columns: true,
-        ltrim: true,
-        })
-    )
-    .on("data", function (row) {
-        // This will push the object row into the array
-        data.push(row);
-    })
-    .on("error", function (error) {
-        console.log(error.message);
-    })
-    .on("end", function () {
-        // Here log the result array
-        console.log("parsed csv data:");
-        console.log(data);
-    });
 
+function Home(props){
     return (
         
         <div class="container">
@@ -41,11 +17,13 @@ function Home(){
                 <Link to="/signup" class="login-button">
 					Sign Up
 				</Link>
+                <div>
+                    {JSON.stringify(props.data)}
+                </div>
             </div>
             <div class="image">
                 <img src={gymnast} alt="gymnastics logo"></img>
             </div>
-
         </div>
     );
 };
