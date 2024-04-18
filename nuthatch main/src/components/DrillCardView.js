50b82de5811223a42d24bfc71fe85bda57f076b1
mayaconway <mayaconway21@augustana.edu>
2024-04-18 14:29:24 -0500

    Started styling card items

--- nuthatch main/src/components/DrillCardView.js
+++ nuthatch main/src/components/DrillCardView.js
@@ -1,31 +1,23 @@
 import React from "react";
+import "../styles/DrillCardView.css";
 
 const Search = (props) => {
   const cardItem = props.cardItem;
   
   return (
     <div>
+      <section>
+          <div className="cards">
+            <div className="title">
+              <h1>{cardItem.Title}</h1>
+            </div>
 
-      <h2>{cardItem.Title}</h2>
+            <img src={cardItem.thumbnailURL} />
 
-      <img src={cardItem.thumbnailURL} />
-
-      <ul>
-
-        <li><h3>Code</h3>{cardItem.CODE}</li>
-
-        <li><h3>Event</h3>{cardItem.Event}</li>
-
-        <li><h3>Category</h3>{cardItem.Category}</li> 
-
-        <li><h3>Level</h3>{cardItem.Level}</li>
-
-        <li><h3>Equipment</h3>{cardItem.Equipment}</li> 
-
-        <li><h3>Gender</h3>{cardItem.Gender}</li>
-
-      </ul>
-
+            <button className = "btn">Add to plan</button>
+          
+      </div>
+      </section>
     </div>
   );
 };
