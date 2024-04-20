90f129b69122a0c24091680c33f1fc0715defd21
mayaconway <mayaconway21@augustana.edu>
2024-04-20 12:31:50 -0500

    Created expandable cards that when clicked show more detail about the skill

--- nuthatch main/src/components/DrillCardView.js
+++ nuthatch main/src/components/DrillCardView.js
@@ -1,23 +1,46 @@
+// credits to https://www.youtube.com/watch?v=rZ6iu5FQr2c&ab_channel=developedbyed
 import React from "react";
 import "../styles/DrillCardView.css";
+import {motion} from 'framer-motion';
+import {useState} from 'react';
+import { isDOMComponent } from "react-dom/test-utils";
 
 const Search = (props) => {
   const cardItem = props.cardItem;
+  const [isOpen, setIsOpen] = useState(false);
   
   return (
-    <div>
-      <section>
-          <div className="cards">
-            <div className="title">
-              <h1>{cardItem.Title}</h1>
-            </div>
+    <div className="App">
 
-            <img src={cardItem.thumbnailURL} />
-
+          <motion.div 
+            transition={{layout: {duration: 1, type: "spring" }}} 
+            layout
+            onClick={() => setIsOpen(!isOpen)} 
+            className="cards"
+            style={{
+              borderRadius: "1rem",
+              boxShadow: "0px 10px 30px rgba(0,0,0, 0.5)",
+            }}
+            >
+              <motion.h2 layout= "position">{cardItem.Title}</motion.h2>
+              <img src={cardItem.thumbnailURL} />
+              {isOpen && (
+            <motion.div
+                initial={{ opacity: 0}}
+                animate={{ opacity: 1}}
+                transition={{ duration: 1}}
+                className= "expand"
+            >
+              <ul>
+        <li>{cardItem.Title}</li>
+        <li>{cardItem.Event}</li>
+        <li>{cardItem.Category}</li>  
+      </ul>
+            </motion.div>
+             )}
             <button className = "btn">Add to plan</button>
           
-      </div>
-      </section>
+      </motion.div>
     </div>
   );
 };
