418ec17b8d34d25971e47303f76bfba4e9e3af07
LoganHansen20 <113001105+Dirtbike140@users.noreply.github.com>
2024-05-07 08:57:01 -0500

    Added payments

--- nuthatch main/src/firebase.js
+++ nuthatch main/src/firebase.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, collection, addDoc, getDocs  } from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -24,5 +24,6 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export const db = getFirestore(app);
 // Initialize Firebase Authentication and get a reference to the service
+export default db;
 export const auth = getAuth(app);
   
