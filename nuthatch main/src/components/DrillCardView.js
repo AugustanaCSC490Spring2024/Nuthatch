d268c31bbb483f6aa5b31952d48dffe81297572d
mayaconway <mayaconway21@augustana.edu>
2024-05-19 11:42:34 -0500

    Styling card h3

--- nuthatch main/src/components/DrillCardView.js
+++ nuthatch main/src/components/DrillCardView.js
@@ -14,7 +14,7 @@ const DrillCardView = (props) => {
 			(<div className="card-view">
 
 					<motion.div 
-						transition={{layout: {duration: 1, type: "spring" }}} 
+						transition={{layout: {duration: 0.5, type: "spring" }}} 
 						layout
 						onClick={() => setIsOpen(!isOpen)} 
 						className="cards"
@@ -22,23 +22,24 @@ const DrillCardView = (props) => {
 							borderRadius: "1rem",
 						}}
 						>
-							<motion.h2 layout= "position">{cardItem.Title}</motion.h2>
+							<motion.h2 className="card-head" layout= "position">{cardItem.Title}</motion.h2>
 							<img className="thumbnail" src={cardItem.thumbnailURL} alt={cardItem.Title}/>
 							{isOpen && ( 
 						<motion.div
 								initial={{ opacity: 0}}
 								animate={{ opacity: 1}}
-								transition={{ duration: 1}}
+								transition={{ duration: 0.5}}
 								className= "expand"
 						>
 				{/* this is anything in the excel sheet */}
 				{/* <h3>Code: {cardItem.CODE}</h3> */}
 				{/* <h3>Title: {cardItem.Title}</h3> */}
-				<h3>Event: {cardItem.Event}</h3>
+				<h3 className="card-text">Event: {cardItem.Event}</h3>
 				{/* <h3>Category: {cardItem.Category}</h3> */}
-				<h3>Gender: {cardItem.Gender}</h3>
-				<h3>Level: {cardItem.Level}</h3>
-				<h3>Equipment: {cardItem.Equipment}</h3>
+				<h3 className="card-text">Gender: {cardItem.Gender}</h3>
+				<h3 className="card-text">Level: {cardItem.Level}</h3>
+				<h3 className="card-text">Equipment: {cardItem.Equipment}</h3>
+				
 
 
 						</motion.div>
